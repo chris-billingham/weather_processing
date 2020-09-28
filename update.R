21aa860c7c6058b048278707d7a6fd00ab504370
chris-billingham <chris.billingham@gmail.com>
2020-09-28 21:26:20 +0100

    suppress start up messages and logging for airflow

--- update.R
+++ update.R
@@ -1,8 +1,10 @@
-library(tidyverse)
-library(magrittr)
-library(darksky)
-library(lubridate)
-
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(magrittr)
+  library(darksky)
+  library(lubridate)
+})
+print("01. reading in postcode data")
 postcode <- readRDS("data/postcode_letter.rds")
 
 # load in functions
@@ -20,6 +22,7 @@ iterate_postcode <- function(c, level, object) {
 }
 
 # read in the old data
+print("02. reading in old data")
 all_hourly <- readRDS("data/all_hourly.rds")
 all_daily <- readRDS("data/all_daily.rds")
 
@@ -43,10 +46,12 @@ start_date <- as.character(start_date)
 end_date <- as.character(end_date)
 
 # read in the new data
+print(glue("03. getting new data from darksky for {start_date} to {end_date}"))
 update <- pmap(list(postcode$lat, postcode$lon, start_date, end_date), read_dates)
 names(update) <- postcode$letters
 
 # process the new data
+print("04. munging data into correct format")
 new_daily <- pmap_df(list(postcode$letters, "daily", "update"), iterate_postcode)
 new_hourly <- pmap_df(list(postcode$letters, "hourly", "update"), iterate_postcode)
 
@@ -55,6 +60,8 @@ all_hourly <- bind_rows(all_hourly, new_hourly)
 all_daily <- bind_rows(all_daily, new_daily)
 
 # save off the new data
+print("05. saving updated data to hdd")
 saveRDS(all_hourly, "data/all_hourly.rds")
 saveRDS(all_daily, "data/all_daily.rds")
 
+print("06. #fin")
