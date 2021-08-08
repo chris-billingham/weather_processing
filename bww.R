d1e7eb2c33a96dff6baabe707757fefc31e1fa21
chris-billingham <chris.billingham@gmail.com>
2021-08-08 19:36:07 +0100

    namespace all the functions and just library magrittr

--- bww.R
+++ bww.R
@@ -1,79 +1,75 @@
 suppressPackageStartupMessages({
-  library(tidyverse)
-  library(rvest)
-  library(lubridate)
-  library(janitor)
-  library(here)
-  library(logger)
+  library(magrittr)
 })
 
 # read the latest buxton weather watch details
 url <- "https://buxtonweather.co.uk/details.htm"
 
 # get the page
-page <- read_html(url)
+page <- rvest::read_html(url)
 
 # grab table 
-log_info("Reading the latest Buxton Weather Watch Data")
+logger::log_info("Reading the latest Buxton Weather Watch data")
 bww_table <- page %>% 
-  html_table() %>% 
-  pluck(1)
+  rvest::html_table() %>% 
+  purrr::pluck(1)
 
 # find where the Last Updated bit is
-log_info("Process the Data")
+logger::log_info("Process the data")
 last_updated <- bww_table %>% 
-  filter(str_detect(X1, "Last Updated")) %>% 
-  select(X1) %>% as.character()
+  dplyr::filter(stringr::str_detect(X1, "Last Updated")) %>% 
+  dplyr::select(X1) %>% 
+  as.character()
 
 # get the time and the date, looking for "on"
-time <- str_extract(last_updated, ".+?(?= on)")
-date <- str_extract(last_updated, "(?<=on ).*")
+time <- stringr::str_extract(last_updated, ".+?(?= on)")
+date <- stringr::str_extract(last_updated, "(?<=on ).*")
 
 # create the time of obs and time now
-date_time_obs <- dmy_hm(paste0(date, " ", time))
+date_time_obs <- lubridate::dmy_hm(paste0(date, " ", time))
 date_time_now <- Sys.time()
 
 # clean up, this is a art
 bww_final <- bww_table %>%
   # create a flag for whether we find the word "Parameter"
-  mutate(parameter_flag = ifelse(X1 == "Parameter", 1, 0)) %>%
+  dplyr::mutate(parameter_flag = ifelse(X1 == "Parameter", 1, 0)) %>%
   # cumulatively sum that which essentially marks the section from the first Parameter to the second
-  mutate(parameter_sum = cumsum(parameter_flag)) %>%
+  dplyr::mutate(parameter_sum = cumsum(parameter_flag)) %>%
   # filter on section 1
-  filter(parameter_sum == 1) %>%
+  dplyr::filter(parameter_sum == 1) %>%
   # get rid of what we don't need
-  select(-parameter_flag, -parameter_sum) %>%
+  dplyr::select(-parameter_flag, -parameter_sum) %>%
   # select and rename what we actually need
-  select(parameter = X1, value = X2) %>%
+  dplyr::select(parameter = X1, value = X2) %>%
   # and the remove the final Parameter
-  filter(!parameter == "Parameter") %>%
+  dplyr::filter(!parameter == "Parameter") %>%
   # take it wide
-  pivot_wider(names_from = "parameter", values_from = "value") %>%
+  tidyr::pivot_wider(names_from = "parameter", values_from = "value") %>%
   # clean up the names
-  clean_names() %>%
+  janitor::clean_names() %>%
   # add in when the observation is from
-  mutate(obs_datetime = date_time_obs) %>%
+  dplyr::mutate(obs_datetime = date_time_obs) %>%
   # move the obs_datetime to the front because i like it
-  relocate(obs_datetime, .before = wind_direction) %>%
+  dplyr::relocate(obs_datetime, .before = wind_direction) %>%
   # convert everything except these three to numeric
-  mutate(across(!c("obs_datetime", "wind_direction"), as.numeric)) %>%
+  dplyr::mutate(dplyr::across(!c("obs_datetime", "wind_direction"), as.numeric)) %>%
   # and add in when the data is from :sunglasses:
-  mutate(date_time_data = date_time_now)
+  dplyr::mutate(date_time_data = date_time_now)
 
 # read in the old data
-log_info("Reading in the old data")
-bww_old <- readRDS(here("data/buxton_weather_watch.rds"))
+logger::log_info("Reading in the old data")
+bww_old <- readRDS(here::here("data/buxton_weather_watch.rds"))
 
 # bind to the new
-bww_new <- bind_rows(bww_old, bww_final)
+bww_new <- dplyr::bind_rows(bww_old, bww_final)
 
 # remove dupes
-log_info("Removing duplicate entries")
+logger::log_info("Removing duplicate entries")
 bww_new_dupes <- bww_new %>%
-  group_by(obs_datetime) %>%
-  filter(date_time_data == min(date_time_data)) %>%
-  ungroup()
+  dplyr::group_by(obs_datetime) %>%
+  dplyr::filter(date_time_data == min(date_time_data)) %>%
+  dplyr::ungroup()
 
 # and save off
-log_info("Saving updated data to disk and #fin")
+logger::log_info("Saving updated data to disk and #fin")
 saveRDS(bww_new_dupes, here("data/buxton_weather_watch.rds"))
